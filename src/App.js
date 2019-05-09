ab832790f1ca2c968261985b280a394c19ff18db
GladysHuancaLu <gladys.luana123@gmail.com>
2019-05-09 06:46:20 -0500

    css de step1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 // import './App.css';
 import Loan from './components/Loan/Loan.js';
+import Home from './components/Home/Home.js';
+
 
 
 class App extends Component {
 
       render() {
           return(
-            <Loan/>
+            <div>
+              <Home/>
+              <Loan/>
+            </div>
+            
           )
       }
   }
--- src/components/Loan/Step1/Step1.js
+++ src/components/Loan/Step1/Step1.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-
+import './Step1.css';
 class App extends Component {
 
     render() {
         return(
             <div>
-                 <div class="text-center mt-5">
-                <h3 class="font-weight-bold">HOLA EMPRENDEDORA,</h3>
-                <h5 class="font-weight text-center mt-3">Te otorgamos un préstamo para que puedas seguir creciendo</h5>
-                <div class="container mt-5">
+                <div class="text-center mt-5">
+                 <h3 class="font-weight-bold mb-4">HOLA EMPRENDEDORA,</h3>
+                 <h5 class="font-weight text-center mt-3">Te otorgamos un préstamo para que puedas seguir creciendo</h5>
+                <div class="container mt-4">
                     <div class="row justify-content-center">
-                    <img src="https://i.ibb.co/0DCHVbV/barra-de-pasos.png"></img>
+                    <img className="mb-5 mt-4" src="https://i.ibb.co/0DCHVbV/barra-de-pasos.png"></img>
 
                     </div>
                 </div>
@@ -18,29 +18,29 @@ class App extends Component {
 
 
                 <div className="container my-5">
-                <p class="title"> Calcula tu préstamo</p>
-                <div className="container mb-5">
+                <p class="title color-base text-link m-5"> Calcula tu préstamo</p>
+                <div className="container mt-20">
                     <div className="row  justify-content-around">
                         <div className="col-md-3">
-                             <p>¿Cúanto dinero necesitas?</p>
+                             <p className="questions">¿Cúanto dinero necesitas?</p>
                              <div className="container">
                                 <div className="row justify-content-between">
-                                <div className="col-2 d-flex justify-content-center align-items-center"
+                                <div className="col-2 color-patito d-flex justify-content-center align-items-center"
                                 onClick={() => {this.props.addMoney(this.props.quantity)}}>
-                                -
+                                <p className="bold-white">-</p>
                                 </div>
-                                <div className="col-7 d-flex justify-content-center align-items-center">S/
+                                <div className="col-7 color-div d-flex justify-content-center align-items-center">S/
                                      {this.props.quantity}</div>
-                                <div className="col-2 d-flex justify-content-center align-items-center"
+                                <div className="col-2 color-patito d-flex justify-content-center align-items-center"
                                 onClick={() => {this.props.subtractMoney(this.props.quantity)}}>
-                                +
+                                <p className="bold-white ">+</p>
                                 </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-3">
-                            <p>¿Cuánto tiempo?</p>
-                            <select className="form-control"
+                            <p className="questions">¿Cuánto tiempo?</p>
+                            <select className=" color-div form-control"
                             name="time"
                             value={this.props.time}
                             onChange={this.props.onChange}
@@ -54,14 +54,15 @@ class App extends Component {
                             </select>
                         </div>
                         <div className="col-md-3">
-                            <p>Debo pagar</p>
-                            <span>{(this.props.quantity*1.12/this.props.time).toFixed(2)}</span>
+                            <p className="questions">Debo pagar</p>
+                            <span className="font-weight-bold display-4">S/.{(this.props.quantity*1.12/this.props.time).toFixed(2)}</span>
                         </div>
                         <div className="col-md-3">
-                            <p>Resumen de Pago</p>
+                            <p className="questions">Resumen de Pago</p>
+                            <small>
                             <div className="container mb-5">
-                                <div className="row justify-content-around">
-                                    <div className=" col-md-1">
+                                <div className="row color-div justify-content-around">
+                                    <div className=" col-md-1 p-0">
                                          <div class="d-flex flex-column">
                                              <div class="p-2">Préstamo</div>
                                              <div class="p-2">{this.props.quantity}</div>
@@ -85,13 +86,34 @@ class App extends Component {
                                 
 
                             </div>
+                            </small>
                         </div>
 
                     </div>
 
                 </div>
 
+              </div>
 
+              <div className="container"> 
+                <div className="row  text center justify-content-around">
+                    <div className=" col-md-8">
+                        <span>Préstamo basado en TCEA 12%</span>
+                        <div mt-5>
+                            <h5>IMPORTANTE</h5>
+                            <p className="color-div">El préstamo será otorgado por la entidad financiera elegida. Juntas y VisNet no es respnsable de la gestión de crédito</p>
+                        </div>
+                    </div>
+                    <div className=" col-md-4 d-flex justify-content-end align-items-center ">
+                      <div>
+                         <h5>Ver cronograma</h5>
+                      </div>
+                      <div>
+                         <button className="btn bg-base btn-lg">SOLICITAR</button>    
+                    </div>
+                    </div>
+                   
+                </div>
               </div>
             </div>
               
