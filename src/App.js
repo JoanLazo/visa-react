c283d08c36e34f2fd66a6c5934a4896003095d2d
lmyesther <esthercheremo@gmail.com>
2019-05-09 18:59:14 -0500

    limpiando

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
-import Home from './components/Home/Home';
-
-const app = () => (
-  <Layout>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/network-contact" component={} />
-      <Route path="/lending" component={} />
-    </Switch>
-  </Layout>
-);
-
-export default app;
