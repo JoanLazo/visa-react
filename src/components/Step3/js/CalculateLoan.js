e816302075ee8d3798ff1aae4eb90987d76c720c
sofianeyra <sofiacristina823@hotmai.com>
2019-05-09 17:09:07 -0500

    estilos de prestamo

--- src/components/Step3/js/CalculateLoan.js
+++ src/components/Step3/js/CalculateLoan.js
@@ -1,31 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './calculateLoan.css';
-import Layout from './layout';
 
 const getLoan = ({payment, getPayment, handleChange, calculatePayment, value, setViews }) => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/#" >MUJER EMPRENDEDORA</a>      
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/prestamo">PIDE PRÉSTAMO</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contactos">RED DE CONTACTOS</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/negocio">HAZ CRECER TU NEGOCIO</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <Layout />
     <p className="pink ml-5 mb-4 mt-5">Calcula tu préstamo</p>
     <div>
       <span className="ml-5">¿Cuánto dinero necesitas?</span>
