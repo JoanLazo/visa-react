b19bb923c91b66aae582e92b9fda77b1fd36db87
lmyesther <esthercheremo@gmail.com>
2019-05-09 12:44:49 -0500

    arreglo en las rutas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
 import Home from './components/Home/Home';
+import Lending from './components/Lending';
 
 const app = () => (
-  <Layout>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/network-contact" component={} />
-      <Route path="/lending" component={} />
-    </Switch>
-  </Layout>
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/lending" component={Lending} />
+  </Switch>
 );
 
 export default app;
