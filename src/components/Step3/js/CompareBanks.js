e448c88947be211d243cbb0a2c76e00026b5e26a
Sofia Neyra <safirokpoper@gmail.com>
2019-05-10 00:21:03 -0500

    Pintado de data est{atica en comparar bancos

--- src/components/Step3/js/CompareBanks.js
+++ src/components/Step3/js/CompareBanks.js
@@ -1,47 +1,106 @@
-import React, { useState } from 'react';
-import listOfBanks from './CardBanks';
-import getLoan from './CalculateLoan';
+import React, { Component } from 'react';
+import ListOfBanks from './CardBanks';
 
-const BankOptions = () => {
-    const [payment, getPayment] = useState(1000);
-    const [value, setValue] = useState('12');
-    const [views, setViews] = useState('getLoan');
+class CompareBanks extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "banks": {
+                "list": [
+                    {
+                        name: "PRESTAMYPE",
+                        img: "https://ibb.co/tKxVqX0",
+                        loan: 10000,
+                        tcea: "12%",
+                        payment: 377.78,
+                        interest: 3600,
+                        total: 13600,
+                    },
+                    {
+                        name: "CAJA HUANCAYO",
+                        img: "https://ibb.co/QQMbQW5",
+                        loan: 10000,
+                        tcea: "20%",
+                        payment: 444.44,
+                        interest: 6000,
+                        total: 16000,
+                    },
+                    {
+                        name: "BCP",
+                        img: "https://ibb.co/KKxfzK1",
+                        loan: 10000,
+                        tcea: "20%",
+                        payment: 444.44,
+                        interest: 6000,
+                        total: 16000,
+                    },
+                    {
+                        name: "CAJA AREQUIPA",
+                        img: "https://ibb.co/4sssPKd",
+                        loan: 10000,
+                        tcea: "25%",
+                        payment: 486.11,
+                        interest: 7500,
+                        total: 17500,
+                    }
+                ],
+            },
+            options: "list"
+        } 
+    }
 
-    const calculatePayment = (values) => {
-        const interest = values * (Number(value)/100);
-        const total = values + interest;
-        const payment = total/Number(value);
-        return [interest, total.toFixed(2), payment.toFixed(2)];
-    };
+    handleChange = (e) => {
+        this.setState({
+          options: e.target.name
+        })
+      }
 
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    };
-
-    const sections = [  
-        {
-          name: 'listOfBanks', 
-          component: <listOfBanks
-            payment={payment}
-            getPayment={getPayment}
-            handleChange={handleChange}
-            calculatePayment={calculatePayment}
-            value={value}
-            setViews={setViews}
-          />
-        },  
-    ]; 
-
-    const renderComponents = index => (sections.map(item => item.component)[index]);
+    addMoney = (todo) => {
+        this.setState({
+          quantity: this.state.quantity -= 1000,
+        })
+    }
     
-    return (
-      <>
-        <SequenceController 
-          sections={sections.length}
-          actualSection={sections.map(item => item.name).indexOf(views)}
-        />
-        {renderComponents(sections.map(item => item.name).indexOf(views))}
-      </>
-    );
-};
-export default BankOptions;
+    subtractMoney = (todo) => {
+        this.setState({
+          quantity: this.state.quantity += 1000,
+        })
+    }
+
+    onChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  
+
+    render() {
+        const {options, banks } = this.state;
+        const size = Object.keys(banks);
+        return (
+            <div className = "section">
+            <h3>¡FELICITACIONES ANITA!</h3>
+            <h2>TE OFRECEMOS LOS MEJORES PRÉSTAMOS PARA TI</h2>
+            <div className="menu-filters color-gray">
+            <p>MONTO</p>
+            <p>TIEMPO</p>
+            </div>
+            <input type="number" min="1000" max="10000" className="filter text-center color-gray">
+            </input>
+            <select name="select filter">
+            <option value="12">12 MESES</option> 
+            <option value="24" selected>24 MESES</option>
+            <option value="36">36 MESES</option>
+            </select>
+            <div className="row">
+            { size.length ?
+            banks[options].map(({ name, img,loan, payment, tcea, interest, total}) => 
+            <ListOfBanks name={name} image={img} tcea={tcea} loan={loan} payment={payment} interest={interest} total={total}/>)
+            : <span className= "ml-3"> Cargando lista de bancos...</span>}
+            </div>
+            </div>
+        )
+    }
+}
+
+export default CompareBanks;
